b89f97d74244239368bd13d9af746a2a5b73d7c6
AlexKimov <alex.ea.li@yandex.ru>
2019-03-09 22:44:48 +0300

    Minor fixes

--- scripts/noesis/fmt_ht_rmp_res.py
+++ scripts/noesis/fmt_ht_rmp_res.py
@@ -4,10 +4,7 @@ import os
 def registerNoesisTypes():
     handle = noesis.register("Hard Truck 1/2 Resource archive", ".res;.rmp")
     noesis.setHandlerExtractArc(handle, resExtractRes)
-    
-    #handle = noesis.register("Hard Truck 1/2 Textures", ".msk;.res")
-    #noesis.setHandlerTypeCheck(handle, artCheckType)
-    #noesis.setHandlerLoadRGBA(handle, artLoadRGBA)    
+     
     return 1
     
  
@@ -18,24 +15,32 @@ def resExtractRes(fileName, fileLen, justChecking):
         fileSections = ("PALETTEFILES", "SOUNDFILES", "TEXTUREFILES", \
                 "BACKFILES", "MASKFILES") 
                 
+        propertySections = ("COLORS", "MATERIALS", "SOUNDS")                
+                            
         filereader = NoeBitStream(f.read());
          
         proStrings = []
          
         while filereader.tell() < filereader.getSize():
-            name, count = filereader.readString().split(" ")
-            proStrings.append(name + '\n')
+            str = filereader.readString()
+            proStrings.append(str + '\n')
+            name, count = str.split(" ")            
             if name in fileSections:
                 for x in range(int(count)):
                     fname = filereader.readString()
+                    proStrings.append(fname + '\n')
+                    
                     size = filereader.readUInt()               
                     fdata = filereader.readBytes(size)
-                    print("Extracting " + fname)
-                    rapi.exportArchiveFile(fname, fdata)
-                    proStrings.append(fname + '\n')
-            else: 
+                    
+                    print("Extracting " + fname.split(" ")[0])
+                    
+                    rapi.exportArchiveFile(fname.split(" ")[0], fdata)
+            elif name in propertySections: 
                 for x in range(int(count)):                  
                     proStrings.append(filereader.readString() + '\n')
+            else:
+                noesis.doException("Error: Wrong format.")
                     
     # write .pro file
     proFilename = rapi.getOutputName() + os.path.basename(fileName) + '.pro'
